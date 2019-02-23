0c4e4af85140212a767327ab2c9ed35910da5c06
DDarrow123 <ddarrowphl@gmail.com>
2019-02-23 17:19:10 -0500

    almost finished top nav bar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const navContainer = document.querySelector(".nav-container");
+const siteName = document.querySelector(".site-name");
 
 navContainer.addEventListener("mouseover", event => {
+  console.log(event.target);
+  // debugger;
   if (
     event.target.children[0] &&
     event.target.children[0].className &&
@@ -12,9 +15,20 @@ navContainer.addEventListener("mouseover", event => {
     console.log(event.target);
     event.target.style.textDecorationLine = "underline";
   }
+  if (
+    event.target.children[0] &&
+    event.target.children[0].className &&
+    event.target.children[0].className === "site-name"
+  ) {
+    console.log(event.target);
+    // debugger;
+    // const siteName = document.querySelector(".site-name");
+    siteName.innerHTML = "Hello!";
+  }
 });
 
 navContainer.addEventListener("mouseout", event => {
   // navSiteName.innerHTML = "avini<span>&#248</span>n";
   event.target.style.textDecoration = "none";
+  siteName.innerHTML = "avini<span>&#248</span>n";
 });
